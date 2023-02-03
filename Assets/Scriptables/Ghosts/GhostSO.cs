56ab27f28c5bb020e5e66b6a7dc81fe0454cab31
Alex Wojtowicz <izabella.wojtowicz@gmail.com>
2023-02-03 11:08:07 +0100

    17

---
+++ Assets/Scriptables/Ghosts/GhostSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ghost", menuName = "ScriptableObjects/Building Ghosts")]
+public class GhostSO : ScriptableObject
+{
+    public string nameString;
+    public Transform visual;
+
+    
+}
